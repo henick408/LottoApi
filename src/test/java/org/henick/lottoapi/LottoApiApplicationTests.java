514175fc7debf79b697d65fab3c16a666ba9ad4f
henick408 <marcel.snopkowski@gmail.com>
2026-06-28 16:04:51 +0200

    Initial commit

---
+++ src/test/java/org/henick/lottoapi/LottoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.henick.lottoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
