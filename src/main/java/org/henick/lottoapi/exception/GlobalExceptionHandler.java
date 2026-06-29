23fb47fda47db73afc2387253573f00862c8ae19
henick408 <marcel.snopkowski@gmail.com>
2026-06-29 17:47:00 +0200

    Exception handling

---
+++ src/main/java/org/henick/lottoapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package org.henick.lottoapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DrawNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleDrawNotFound(DrawNotFoundException exception) {
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.NOT_FOUND,
+                exception.getMessage()
+        );
+        return ResponseEntity.status(response.status()).body(response);
+    }
+
+}
