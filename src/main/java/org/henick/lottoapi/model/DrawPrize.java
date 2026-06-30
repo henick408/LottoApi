d8e9a67154d3b9b2585c6a01dd785054e160ea7d
henick408 <marcel.snopkowski@gmail.com>
2026-06-30 12:49:07 +0200

    Id in DrawPrize

--- src/main/java/org/henick/lottoapi/model/DrawPrize.java
+++ src/main/java/org/henick/lottoapi/model/DrawPrize.java
@@ -7,11 +7,13 @@ import java.util.List;
 
 @Getter
 public class DrawPrize {
+    long id;
     GameType gameType;
     OffsetDateTime drawDate;
     List<Prize> prizes;
 
-    public DrawPrize(GameType gameType, OffsetDateTime drawDate, List<Prize> prizes) {
+    public DrawPrize(long id, GameType gameType, OffsetDateTime drawDate, List<Prize> prizes) {
+        this.id = id;
         this.gameType = gameType;
         this.drawDate = drawDate;
         this.prizes = prizes;
