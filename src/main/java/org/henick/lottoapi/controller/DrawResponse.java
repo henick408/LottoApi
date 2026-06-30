c353302befa8de4fe8188eff66ff686f3b5d64a6
henick408 <marcel.snopkowski@gmail.com>
2026-06-30 13:04:16 +0200

    Mapper for draws and prizes

--- src/main/java/org/henick/lottoapi/controller/DrawResponse.java
+++ src/main/java/org/henick/lottoapi/controller/DrawResponse.java
@@ -1,10 +1,12 @@
 package org.henick.lottoapi.controller;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.henick.lottoapi.model.GameType;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record DrawResponse(
         long drawSystemId,
         OffsetDateTime drawDate,
